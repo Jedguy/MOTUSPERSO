40bc3f8623a24071d67ce7ebddc8063ebabb53b5
doreau <doreau@HOS2UC18.sio.fr>
2021-03-17 11:44:36 +0100

    nbr aléatoire réalisé, organisation des mots

--- test-csv/src/Principal.java
+++ test-csv/src/Principal.java
@@ -1,35 +1,51 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
-
 public class Principal {
-	
+
 	public static void main(String[] args) {
+
 		String fileName = "animaux.csv";
 		File file = new File(fileName);
-		
+
+		Random r = new Random();
+		ArrayList<String> listS = new ArrayList<String>();
+
 		try {
 			Scanner inputStream = new Scanner(file);
-			inputStream.nextLine();//ignore la première ligne
+			BufferedReader br = new BufferedReader(new FileReader(fileName));
 			while (inputStream.hasNext()) {
 				String data = inputStream.next();
-				String[] values = data.split(";"); // supprime la virgule
-				//System.out.println(values[0]); //choisi la colonne des mots
-				String x = values[0];
-				int n = 7;
-				
-				for (int i = 0; i < n; i++){
-					String t = x.substring(i,i+1);
-					System.out.println(t);
-				}
+				String[] values = data.split(";");
+				System.out.println(values[0]);
+				listS.add(data);
 			}
-		}
-		catch (FileNotFoundException e) {
+			int nbrMot = listS.size();
+			System.out.println(listS);
+			System.out.println(generateRandomInt(nbrMot));
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+
 		}
-		
-		
+	}
+	public static int generateRandomInt(int nbrMot){
+	    Random random = new Random();
+	    return random.nextInt(nbrMot);
 	}
 
 }
+/*
+ * String data = inputStream.next(); String[] values = data.split(";"); //
+ * supprime la virgule String x = values[0]; int n = 7;
+ * 
+ * for (int i = 0; i < n; i++){ String t = x.substring(i,i+1);
+ * System.out.println(t);
+ */
